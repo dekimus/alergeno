dda425fb11e288cd32d6b815434c458af38a0e02
Israel Perez <deki.wii@gmail.com>
2015-05-07 23:59:44 +0200

    Update alergenos.py
    Añadida una opción mas al menú y depurados algunos errores.

--- alergenos.py
+++ alergenos.py
@@ -1,31 +1,42 @@
-# -*- coding: utf-8 -*-
-import os, sys, csv
 
+# -*- coding: utf-8 -*-
+
+import os, sys, csv
 
 
 
 def __main__():
 
+    """Funcion principal del programa que gestiona el menu principal"""
+
     os.system('clear')
     print "\n#########################################################################"
     print "#Bienvenido al programa de gestion de alergenos y ingredientes. \n#"
     print "#                       Escrito por Israel Perez Valero. \n########################################################################## \n"
-    print "1. Añadir productos al listado de ingredientes. \n2. Crea un archivo con los Productos y alergenos.\n"
-    print "Teclea ESC en mayuscula para terminar... \n"
+    print "1. Añadir productos al listado de ingredientes. \n2. Crea un archivo con los Productos y alergenos."
+    print "3. Mostrar el listado de ingredientes.\n\nTeclea ESC en mayuscula para terminar... \n"
     eleccion = False
     while eleccion == False:
-        eleccion = str(raw_input("Elige tu opcion: "))
+        eleccion = raw_input("Elige tu opcion: ").upper()
         if eleccion == "1":
             os.system('clear')
             eleccion = True
             listaIngredientes()
         elif eleccion == "2":
             os.system('clear')
+            eleccion = True
             listaAlergenos()
         elif eleccion == "ESC":
             os.system('clear')
             print "Gracias por usarme, hasta la proxima."
             sys.exit()
+        elif eleccion == "3":
+            eleccion == True
+            os.system('clear')
+            mostrarArchivo()
+        else:
+            print "Introduce un valor valido por favor\n"
+            eleccion = False
 
 
 
@@ -56,12 +67,22 @@ def listaIngredientes():
     pausa()
 
 def listaAlergenos():
-    fileRead = open('Listadoingredientes.csv', "r")
+    """Genera un archivo.txt con la lista de ingredientes y alergenos"""
+
+    try:
+        fileRead = open('Listadoingredientes.csv', "r")
+    except:
+        print "Listado de ingredientes no encontrado, crea uno primero por favor."
+        pausa()
+
     read = csv.reader(fileRead)
     fileWrite = open('Alergenos.txt', "w")
     for row in read:
         fileWrite.write(row[0]+":\n")
-        fileWrite.write("Alergenos: "+row[2]+"\n\n")
+        for letra in range(len(row[0])):
+            fileWrite.write("--")
+
+        fileWrite.write("\nAlergenos: "+row[2]+"\n\n")
     fileRead.close()
     fileWrite.close()
     print "Archivo creado correctamente.\n"
@@ -77,4 +98,18 @@ def pausa():
         if pausa == "":
             pausa2 = True
     __main__()
+
+def mostrarArchivo():
+    """Funcion encargada de listar el archivo de ingredientes"""
+
+    try:
+        fileRead = open('Listadoingredientes.csv', "r")
+    except:
+        print "Listado de ingredientes no encontrado, genera uno primero por favor"
+        pausa()
+    read = csv.reader(fileRead)
+    for row in read:
+        print "  ".join(row)+"\n"
+    pausa()
+
 __main__()
